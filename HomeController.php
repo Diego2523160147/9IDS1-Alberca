267ceac3997e2dbb2f11be44f8c0c236642fedb9
Edgar2523160009 <2523160009@e.uttecamac.edu.mx>
2025-07-11 08:20:44 -0600

    Actializacion de login Accesos (profesores, administradores, clientes)

--- HomeController.php
+++ HomeController.php
@@ -18,7 +18,7 @@ class HomeController extends Controller
 
     /**
      * Show the application dashboard.
-     *
+     * PRUEBA DE EDITAR
      * @return \Illuminate\Contracts\Support\Renderable
      */
     public function index()
